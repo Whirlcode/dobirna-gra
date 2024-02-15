0941125f7756b1ac0585417ff5931652eabdb4b3
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-16 01:22:35 +0200

    Rename file with impl hub server

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { appStore } from '@app/Store'
 
-import "@app/SignalR/GameHub"
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={appStore}>

2b60d0939b654db7f7512e40798cbb02ddaceee3
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-16 01:21:30 +0200

    Added interfaces for working with the Hub Server

--- src/SignalR/GameHub.ts
+++ src/SignalR/GameHub.ts
@@ -2,34 +2,93 @@ import * as signalR from "@microsoft/signalr";
 import * as msgpack from "@microsoft/signalr-protocol-msgpack"
 import * as Message from "@app/SignalR/MessageTypes"
 
-function getGameServerURL() {
-    const useLocalServer = false;
-    return useLocalServer ? "https://localhost:8000/gamehub" : "https://dobirnagraserver.onrender.com/gamehub"
+export interface IHubObserver {
+    onProfileChanged(lobbyInfo: Message.LobbyInfo): void,
+    onLobbyStateChanged(lobbyInfo: Message.LobbyInfo): void
+    onServerError(lobbyInfo: Message.LobbyInfo): void
 }
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl(getGameServerURL(), {
-        skipNegotiation: true,
-        transport: signalR.HttpTransportType.WebSockets
-    })
-    .withHubProtocol(new msgpack.MessagePackHubProtocol())
-    .withAutomaticReconnect()
-    .build();
+export interface IHubServer {
 
-connection.on("OnProfileChanged", (me : Message.UserInfo) => {
-    console.log(`OnProfileChanged: ${JSON.stringify(me)}`)
-});
+    connectAsync(): Promise<void>
+    disconnectAsync(): Promise<void>
+    getState(): signalR.HubConnectionState
 
-connection.on("OnLobbyStateChanged", (lobbyInfo : Message.LobbyInfo) => {
-    console.log(`OnLobbyStateChanged: ${JSON.stringify(lobbyInfo)}`)
-});
+    subscribe(listiner: IHubObserver): void
+    unsunscribe(listiner: IHubObserver): void
 
-connection.on("OnServerError", (str) => {
-    console.error(str)
-})
+    updateProfileAsync(name?: string): Promise<void>
+    createLobbyAsync(name: string): Promise<void>
+    joinLobbyAsync(inviteCode: string): Promise<void>
+}
 
-connection.start().then(async () => {
-    await connection.send("UpdateProfile", { Name: "Julian"} as Message.UpdateProfileAction)
-    await connection.send("CreateLobby", { Name: "Test" } as Message.CreateLobbyAction);
-    //await connection.send("JoinLobby", { InviteCode: "---" } as Message.JoinLobbyAction);
-})
+class HubServerImpl implements IHubServer {
+
+    connection: signalR.HubConnection
+
+    constructor() {
+        function getGameServerURL() {
+            const useLocalServer = false;
+            return useLocalServer ? "https://localhost:8000/gamehub" : "https://dobirnagraserver.onrender.com/gamehub"
+        }
+
+        this.connection = new signalR.HubConnectionBuilder()
+            .withUrl(getGameServerURL(), {
+                skipNegotiation: true,
+                transport: signalR.HttpTransportType.WebSockets
+            })
+            .withHubProtocol(new msgpack.MessagePackHubProtocol())
+            .withAutomaticReconnect()
+            .build();
+    }
+
+    async connectAsync(): Promise<void> {
+        if (this.connection.state == signalR.HubConnectionState.Disconnected) {
+            console.log(`start connect: ${this.connection.state}`)
+            await this.connection.start()
+            console.log(`finished connect: ${this.connection.state}`)
+        }
+    }
+
+    getState(): signalR.HubConnectionState {
+        return this.connection.state;
+    }
+
+    disconnectAsync(): Promise<void> {
+        return this.connection.stop()
+    }
+
+    subscribe(listiner: IHubObserver): void {
+        this.connection.on("OnProfileChanged", listiner.onProfileChanged);
+        this.connection.on("OnLobbyStateChanged", listiner.onLobbyStateChanged);
+        this.connection.on("OnServerError", listiner.onServerError);
+    }
+
+    unsunscribe(listiner: IHubObserver): void {
+        this.connection.off("OnProfileChanged", listiner.onProfileChanged);
+        this.connection.off("OnLobbyStateChanged", listiner.onLobbyStateChanged);
+        this.connection.off("OnServerError", listiner.onServerError);
+    }
+
+    async updateProfileAsync(name?: string | undefined): Promise<void> {
+        await this.connection.send("UpdateProfile", {
+            Name: name
+        } as Message.UpdateProfileAction)
+    }
+
+    async createLobbyAsync(name: string): Promise<void> {
+        await this.connection.send("CreateLobby", {
+            Name: name
+        } as Message.CreateLobbyAction);
+    }
+
+    async joinLobbyAsync(inviteCode: string): Promise<void> {
+        await this.connection.send("JoinLobby", {
+            InviteCode: inviteCode
+        } as Message.JoinLobbyAction);
+    }
+}
+
+const hubServer: IHubServer = new HubServerImpl();
+
+export default hubServer;

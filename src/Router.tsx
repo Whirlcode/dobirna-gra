9def83d3451079f7e1b20c55e982e329f87d2659
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-18 21:55:36 +0200

    Fix basename url for gh-pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -22,4 +22,4 @@ export const router = createBrowserRouter([
             }
         ]
     }
-]);
+], { basename: import.meta.env.BASE_URL});
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dobirna-gra",
   plugins: [react()],
   server: {
     port: 8080

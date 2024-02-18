2b68bf7f03f475ae9f848d4d95cfa774d87b6402
Alex <a211019971420125@gmail.com>
2024-02-18 23:26:51 +0200

    Fixed kebab error, fixed key problem

--- src/components/gameScene/playerComp/PlayersLayout.tsx
+++ src/components/gameScene/playerComp/PlayersLayout.tsx
@@ -6,37 +6,37 @@ import EmptyPlayerPlace from "@app/components/gameScene/playerComp/emptyPlayerPl
 export default function PlayersLayout() {
   const Places = useAppSelector(s => s.gameState.lobby?.Places)
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          position: "relative",
-          padding: "10px",
-          borderRadius: 25,
-          width: "100%",
-          gap: 4,
-          flexWrap: "wrap",
-        }}
-      >
-        {Places!.map((user, idx) => {
-          return (
-            <>
-              {user.IsOccupied
-                ?
-                <PlayerCard
-                  key={user.UserId}
-                  initialVal={60000}
-                  step={900}
-                  playerName={user.UserName}
-                />
-                :
-                <EmptyPlayerPlace seatIdx={idx} />
-              }
-            </>
-          );
-        })}
-      </Box>
-    </>
+    <Box
+      key={Math.floor(Math.random() * 9999)}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        position: "relative",
+        padding: "10px",
+        borderRadius: 25,
+        width: "100%",
+        gap: 4,
+        flexWrap: "wrap",
+      }}
+    >
+      {Places?.map((user, idx) => {
+        return (
+          <Box key={idx}>
+            {user.IsOccupied
+              ?
+              <PlayerCard
+                key={user.UserId}
+                initialVal={60000}
+                step={900}
+                playerName={user.UserName}
+                scoreOfPlace={user.Score}
+              />
+              :
+              <EmptyPlayerPlace key={user.UserId} seatIdx={idx} />
+            }
+          </Box>
+        );
+      }) ?? []}
+    </Box>
   );
 }

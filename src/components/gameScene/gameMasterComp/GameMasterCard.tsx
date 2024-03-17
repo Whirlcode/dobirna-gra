ee0b75cc590bf7b90e038615869ac34ecd604250
Alex <a211019971420125@gmail.com>
2024-03-18 00:07:01 +0200

    Moved to separate components game master`s control panel

--- src/components/gameScene/gameMasterComp/GameMasterCard.tsx
+++ src/components/gameScene/gameMasterComp/GameMasterCard.tsx
@@ -1,11 +1,7 @@
-import { AspectRatio, Box, Button, ButtonGroup, Card, Snackbar, Tooltip, Typography } from "@mui/joy";
+import { AspectRatio, Box, Card, Typography } from "@mui/joy";
 import { useAppSelector } from "@app/Store";
-import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
-import SkipNextRoundedIcon from '@mui/icons-material/SkipNextRounded';
-import PauseRoundedIcon from '@mui/icons-material/PauseRounded';
-import KeyboardTabRoundedIcon from '@mui/icons-material/KeyboardTabRounded';
-import { useState } from "react";
 import DefaultUserImage from '@app/assets/maxresdefault.jpg'
+import ControlGameFlowButtons from "./ControlGameFlowButtons";
 
 export default function GameMasterCard() {
   const amMaster = useAppSelector((s) => s.gameState.amMaster);
@@ -60,49 +56,6 @@ export default function GameMasterCard() {
   );
 }
 
-function ControlGameFlowButtons() {
-  const [isPaused, setIsPaused] = useState(false)
-  const [openModalSkipRound, setOpenModalSkipRound] = useState(false)
-
-  const handlePauseClick = () => {
-    setIsPaused((v) => !v)
-  }
-
-  return (
-    <>
-      <ButtonGroup
-        orientation="vertical"
-        aria-label="vertical solid button group"
-        variant="soft"
-        size="lg"
-        sx={{ '--ButtonGroup-separatorColor': 'rgba(160, 234, 255, 0.5)' }}
-      >
-        <Tooltip placement="left" size="lg" title="Pause / Unpause">
-          <Button key="pause" sx={{ height: "4vw", padding: '0' }} onClick={handlePauseClick}>
-            {isPaused
-              ?
-              <PlayArrowRoundedIcon color="success" sx={{ fontSize: '50px' }} />
-              :
-              <PauseRoundedIcon color="success" sx={{ fontSize: '50px' }} />
-            }
-          </Button>
-        </Tooltip>
-        <Tooltip placement="left" size="lg" title="Skip question">
-          <Button key="skipQuestion" sx={{ height: "4vw", padding: '0' }}>
-            <SkipNextRoundedIcon color="warning" sx={{ fontSize: '50px' }} />
-          </Button>
-        </Tooltip>
-        <Tooltip placement="left" size="lg" title="Skip Round">
-          <Button onClick={() => setOpenModalSkipRound((v) => !v)} key="skipRound" sx={{ height: "4vw", padding: '0' }}>
-            <KeyboardTabRoundedIcon sx={{ fontSize: '50px', color: 'rgb(255, 86, 86)' }} />
-          </Button>
-        </Tooltip>
-      </ButtonGroup>
-      <ModalForSkipRound open={openModalSkipRound} />
-    </>
-  )
-}
-
 function TextOfGameMaster() {
   return (
     <>
@@ -133,46 +86,3 @@ function TextOfGameMaster() {
 }
 
 
-function ModalForSkipRound({ open }: { open: boolean }) {
-  return <>
-    <Snackbar
-      variant="soft"
-      anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
-      open={open}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '200px',
-        padding: '20px',
-        justifyContent: 'space-evenly'
-      }}>
-      <Box>
-        <Typography level="h2">Do you want to skip this round?</Typography>
-      </Box>
-      <Box>
-        <ButtonGroup
-          sx={{ width: '500px' }}
-          buttonFlex={1}
-          aria-label="flex button group"
-        >
-          <Button sx={{
-            ":hover": { boxShadow: "0px 0px 20px 10px rgb(130, 255, 146)" },
-            fontSize: '18px'
-          }}
-            color="success"
-            variant="soft"
-          >Yes
-          </Button>
-          <Button sx={{
-            ":hover": { boxShadow: "0px 0px 20px 10px rgb(255, 112, 112)" },
-            fontSize: '18px'
-          }}
-            color="danger"
-            variant="soft"
-          >No
-          </Button>
-        </ButtonGroup>
-      </Box>
-    </Snackbar>
-  </>
-}
